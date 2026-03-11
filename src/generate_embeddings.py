6a84fe734bb0cfcca143aacac260c89da1e39dfd
Noa_TEDGHI <Noatedghi@gmail.com>
2026-03-11 18:14:18 +0100

    embedding ok

--- src/generate_embeddings.py
+++ src/generate_embeddings.py
@@ -1,48 +1,48 @@
-import sys
 import os
 import json
 from sentence_transformers import SentenceTransformer
 from preprocessing import clean_text
 
-# Ajouter le chemin racine pour trouver src
-sys.path.append(os.path.abspath("."))
+ 
+# Définir le chemin racine du projet
+ 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Fichiers
-raw_file = "/data/raw/jobs_raw.json"
-clean_file = "/data/processed/jobs_clean.json"
-embeddings_file = "/data/processed/jobs_embeddings.json"
+raw_file = os.path.join(BASE_DIR, "data", "raw", "jobs_raw.json")
+clean_file = os.path.join(BASE_DIR, "data", "processed", "jobs_clean.json")
+embeddings_file = os.path.join(BASE_DIR, "data", "processed", "jobs_embeddings.json")
 
-os.makedirs("/data/processed", exist_ok=True)
+os.makedirs(os.path.join(BASE_DIR, "data", "processed"), exist_ok=True)
 
-# -------------------------------
-# 1️⃣ Lire le JSON brut
-# -------------------------------
+ 
+# Lire le JSON brut
+ 
 with open(raw_file, encoding="utf-8") as f:
     jobs = json.load(f)
 
-# -------------------------------
-# 2️⃣ Nettoyer les descriptions
-# -------------------------------
+print(f"{len(jobs)} offres chargées")
+
+# Nettoyer les descriptions
+ 
 for job in jobs:
     job["clean_description"] = clean_text(job["description"])
 
-# Sauvegarder les descriptions nettoyées
 with open(clean_file, "w", encoding="utf-8") as f:
     json.dump(jobs, f, ensure_ascii=False, indent=2)
 
-print(f"{len(jobs)} descriptions nettoyées et sauvegardées dans {clean_file}")
+print("Descriptions nettoyées sauvegardées")
+ 
+# Charger le modèle embeddings
 
-# -------------------------------
-# 3️⃣ Générer les embeddings
-# -------------------------------
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+# Générer et sauvegarder les embeddings
 
 for job in jobs:
     job["embedding"] = model.encode(job["clean_description"]).tolist()
 
-# Sauvegarder les embeddings
 with open(embeddings_file, "w", encoding="utf-8") as f:
     json.dump(jobs, f, ensure_ascii=False, indent=2)
 
-print(f"Embeddings générés et sauvegardés dans {embeddings_file}")
+print("Embeddings générés et sauvegardés")
